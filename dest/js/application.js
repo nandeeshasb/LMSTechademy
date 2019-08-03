1190f541dd7afdb2fd7f8e99a19b64ae04e2371d
nandeesha <nandeesha.sb@iiht.co.in>
2019-08-02 18:47:29 -0700

    updated menu bar

--- dest/js/application.js
+++ dest/js/application.js
@@ -1,8 +1,27 @@
-(function($) {
+/*(function($) {
     "use strict";
 
     // manual carousel controls
     $('.next').click(function(){ $('.carousel').carousel('next');return false; });
     $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
     
-})(jQuery);
+})(jQuery);*/
+
+$(document).ready(main);
+var contador=1;
+$function main(){
+	$('.menu-bar').click(function(){
+		//$('nav').toggle();
+		if (contador == 1) {
+			$('nav').animate({
+				left:'0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left:'-100%'
+			});
+		}
+	});
+}
