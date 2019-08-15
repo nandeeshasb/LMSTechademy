87cd580268eceb4a6fb491fdf36ba55959ec967d
nandeesha <nandeesha.sb@iiht.co.in>
2019-08-14 18:28:02 -0700

    adding file

--- src/js/application.js
+++ src/js/application.js
@@ -1,27 +1,7 @@
-/*(function($) {
-    "use strict";
 
-    // manual carousel controls
-    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
-    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
-    
-})(jQuery);*/
-
-$(document).ready(main);
-var contador = 1;
-$function main(){
-	$('.menu-bar').click(function(){
-		//$('nav').toggle();
-		if (contador == 1) {
-			$('nav').animate({
-				left:'0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left:'-100%'
-			});
-		}
+$(function(){
+	$('.dates #user2').datepicker({
+		'format':'dd-mm-yyyy',
+		'autoclose':true
 	});
-}
+});
